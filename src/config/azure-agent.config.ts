05c301e7957da5552696b57ac3af9e488806024a
Sebastian <sebastianvelasquez2807@gmail.com>
2026-02-20 00:32:32 -0500

    prueba de actualización de imagen en container apps

--- src/config/azure-agent.config.ts
+++ src/config/azure-agent.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('azureAgent', () => ({
-  endpoint: process.env.AZURE_AGENT_ENDPOINT,
-  apiKey: process.env.AZURE_AGENT_API_KEY,
-  apiVersion: process.env.AZURE_AGENT_API_VERSION || '2024-12-01-preview',
+  endpoint: process.env.AGENT_ENDPOINT,
+  apiKey: process.env.AGENT_API_KEY,
+  apiVersion: process.env.AGENT_API_VERSION || '2024-12-01-preview',
 }));
